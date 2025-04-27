a443389dbb70c01e30abf84c73daf21d54de6cf5
Pomlp <Pomllpp005@gmail.com>
2025-04-27 15:31:57 +0700

    v1

---
+++ IQFARST.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Arduino.h>
+#include <Servo.h>
+#include <QTRSensors.h>
+
+
+#include "Sensor.h"
+#include "MotorController.h"
+#include "ESCController.h"
+#include "PIDController.h"
+#include "Robot.h"
+#include "QTRSensorsMUX.h"
